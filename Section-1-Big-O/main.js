3b361d39fe22eb6ec60cd85269a585207edbdb55
Jeffrey Luzon <iamjeffrey099@gmail.com>
2023-05-18 20:28:33 -0400

    hello

--- Section-1-Big-O/main.js
+++ Section-1-Big-O/main.js
@@ -71,62 +71,62 @@
 // }];
 
 // console.log(people[0])
-class Node {
-    constructor(value) {
-        this.value = value,
-        this.next = null
-    }
-}
+// class Node {
+//     constructor(value) {
+//         this.value = value,
+//         this.next = null
+//     }
+// }
 
-class LinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next:null 
-        }
-        this.tail = this.head
-        this.length = 1
-    }
-    append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
-        return this
-    }
-    prepend(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
-        newNode.next = this.head;
-        this.head = newNode
-        this.length++;
-        return this
-    }
-    printlist() {
-        const array = []; 
-        let currentNode =this.head;
-        while (currentNode !== null) {
-            array.push(currentNode.value)
-            currentNode = currentNode.next
-        } 
-        return array;
-    }
-    insert(index, value) {
-        if (index >= this.length) {
-            return this.append(value)
-        }
-        const newNode = {
-            value: value,
-            next: null
-        }
+// class LinkedList {
+//     constructor(value) {
+//         this.head = {
+//             value: value,
+//             next:null 
+//         }
+//         this.tail = this.head
+//         this.length = 1
+//     }
+//     append(value) {
+//         const newNode = {
+//             value: value,
+//             next: null
+//         }
+//         this.tail.next = newNode;
+//         this.tail = newNode;
+//         this.length++;
+//         return this
+//     }
+//     prepend(value) {
+//         const newNode = {
+//             value: value,
+//             next: null
+//         }
+//         newNode.next = this.head;
+//         this.head = newNode
+//         this.length++;
+//         return this
+//     }
+//     printlist() {
+//         const array = []; 
+//         let currentNode =this.head;
+//         while (currentNode !== null) {
+//             array.push(currentNode.value)
+//             currentNode = currentNode.next
+//         } 
+//         return array;
+//     }
+//     insert(index, value) {
+//         if (index >= this.length) {
+//             return this.append(value)
+//         }
+//         const newNode = {
+//             value: value,
+//             next: null
+//         }
 
-    }
-}
+//     }
+// }
 
 function twoSum(nums, target) {
     for(let i=0; i < nums.length; i++) {
